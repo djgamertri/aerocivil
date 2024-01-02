430e5ac01ad594e9723b2fc8f5148a5665808b98
djgamertri_ <djgamertri@gmail.com>
2024-01-02 13:19:06 -0500

    UserCard Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
+import Canvas from './components/canva'
+import CardUser from './components/cardUser/CardUser'
 
-function App() {
-
+function App () {
   return (
     <>
       <h1>Vite + React</h1>
+      <Canvas />
+      <CardUser />
+      <CardUser />
+      <CardUser />
+      <CardUser />
+
     </>
   )
 }
---
+++ src/components/cardUser/CardUser.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './CardUser.css'
+
+function CardUser () {
+  return (
+    <div class='User'>
+      <img src='https://economia3.com/wp-content/uploads/2019/12/Natalia-Juarranz-EQUIPO-HUMANO.jpg' alt='' />
+      <div class='UserText'>
+        <h1>Esmeralda Goméz</h1>
+        <p>Secretarìa General</p>
+      </div>
+    </div>
+  )
+}
+
+export default CardUser
