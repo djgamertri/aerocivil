39d1ed2723e067c11972633e4a45b7d84089589d
djgamertri_ <djgamertri@gmail.com>
2024-01-03 06:13:26 -0500

    Edge Type Step

--- src/App.jsx
+++ src/App.jsx
@@ -13,15 +13,15 @@ import 'reactflow/dist/style.css'
 
 const initialNodes = [
   { id: '1', type: 'User', position: { x: 528, y: -204 }, data: { name: 'Esmeralda Molina Gomez', rol: 'Secretaria General', img: 'https://economia3.com/wp-content/uploads/2019/12/Natalia-Juarranz-EQUIPO-HUMANO.jpg' } },
-  { id: '2', type: 'User', position: { x: 528, y: 102 }, data: { name: 'Jhon Doe', rol: 'Director de Operaciones', img: 'https://economia3.com/wp-content/uploads/2019/12/Natalia-Juarranz-EQUIPO-HUMANO.jpg' } },
-  { id: '3', type: 'User', position: { x: 900, y: 102 }, data: { name: 'Jhon Doe', rol: 'Gerente de Marketing', img: 'https://economia3.com/wp-content/uploads/2019/12/Natalia-Juarranz-EQUIPO-HUMANO.jpg' } },
+  { id: '2', type: 'User', position: { x: 528, y: 112 }, data: { name: 'Jhon Doe', rol: 'Director de Operaciones', img: 'https://economia3.com/wp-content/uploads/2019/12/Natalia-Juarranz-EQUIPO-HUMANO.jpg' } },
+  { id: '3', type: 'User', position: { x: 900, y: 112 }, data: { name: 'Jhon Doe', rol: 'Gerente de Marketing', img: 'https://economia3.com/wp-content/uploads/2019/12/Natalia-Juarranz-EQUIPO-HUMANO.jpg' } },
   { id: '4', type: 'User', position: { x: 528, y: 383 }, data: { name: 'Jhon Doe', rol: 'Gerente de Recursos Humanos', img: 'https://economia3.com/wp-content/uploads/2019/12/Natalia-Juarranz-EQUIPO-HUMANO.jpg' } }
 
 ]
 const initialEdges = [
-  { id: 'SC1-2', source: '1', target: '2' },
-  { id: 'SC1-3', source: '1', target: '3' },
-  { id: 'SC2-4', source: '2', target: '4' }
+  { id: 'SC1-2', type: 'step', source: '1', target: '2' },
+  { id: 'SC1-3', type: 'step', source: '1', target: '3' },
+  { id: 'SC2-4', type: 'step', source: '2', target: '4' }
 
 ]
 
