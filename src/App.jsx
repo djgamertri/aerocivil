9e6428b3ceb0bb528f2e659767f5807db8f8271f
djgamertri_ <djgamertri@gmail.com>
2024-01-04 12:13:31 -0500

    Add Node and Update Edge

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useRef } from 'react'
 import { FaUser } from 'react-icons/fa6'
 import Button from './components/Button/Button'
 import ReactFlow, {
@@ -8,21 +8,42 @@ import ReactFlow, {
   useNodesState,
   useEdgesState,
   addEdge,
-  Panel
+  Panel,
+  updateEdge
 } from 'reactflow'
 import './App.css'
 import 'reactflow/dist/style.css'
-import { initialEdges, initialNodes, nodeTypes } from './assets/data'
+import { edgeTypes, initialEdges, initialNodes, nodeTypes } from './assets/data'
 import Navbar from './components/Navbar/Navbar'
 
 export default function App () {
+  const edgeUpdateSuccessful = useRef(true)
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes)
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges)
+
+  console.log(nodes)
 
   const onConnect = useCallback(
     (params) => setEdges((eds) => addEdge(params, eds)),
     [setEdges]
   )
+
+  const onEdgeUpdateStart = useCallback(() => {
+    edgeUpdateSuccessful.current = false
+  }, [])
+
+  const onEdgeUpdate = useCallback((oldEdge, newConnection) => {
+    edgeUpdateSuccessful.current = true
+    setEdges((els) => updateEdge(oldEdge, newConnection, els))
+  }, [])
+
+  const onEdgeUpdateEnd = useCallback((_, edge) => {
+    if (!edgeUpdateSuccessful.current) {
+      setEdges((eds) => eds.filter((e) => e.id !== edge.id))
+    }
+
+    edgeUpdateSuccessful.current = true
+  }, [])
 
   return (
     <div style={{ width: '100%', height: '100vh' }}>
@@ -32,10 +53,15 @@ export default function App () {
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
+        onEdgeUpdate={onEdgeUpdate}
+        onEdgeUpdateStart={onEdgeUpdateStart}
+        onEdgeUpdateEnd={onEdgeUpdateEnd}
         nodeTypes={nodeTypes}
+        edgeTypes={edgeTypes}
+        fitView
       >
         <Panel position='top-right'><Button icon={<FaUser />} /></Panel>
-        <Navbar />
+        <Navbar setNodes={setNodes} nodes={nodes} />
         <Controls />
         <MiniMap />
         <Background variant='dots' gap={12} size={1} />
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import './Navbar.css'
 import Button from '../Button/Button'
 import { FaUser, FaCirclePlus } from 'react-icons/fa6'
 
 import { Panel } from 'reactflow'
 
-function Navbar () {
+function Navbar ({ setNodes }) {
+  const getNodeId = () => `ID_${+new Date()}`
+
+  const onAdd = useCallback(() => {
+    const newNode = {
+      id: getNodeId(),
+      type: 'User',
+      data: { label: 'Added node' },
+      position: {
+        x: Math.random() * window.innerWidth - 100,
+        y: Math.random() * window.innerHeight
+      }
+    }
+    setNodes((nds) => nds.concat(newNode))
+  }, [setNodes])
   return (
     <Panel position='top-center'>
       <div className='nav'>
-        <Button icon={<FaCirclePlus />} />
+        <Button icon={<FaCirclePlus />} onClick={onAdd} />
         <Button icon={<FaCirclePlus />} />
         <Button icon={<FaUser />} />
       </div>
