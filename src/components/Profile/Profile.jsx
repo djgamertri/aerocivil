1e43aca1c2e02f1a723df250c30898d5810e34b8
djgamertri <95003267+djgamertri@users.noreply.github.com>
2024-01-09 09:31:55 -0500

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
-import JSONPretty from 'react-json-pretty'
-import 'react-json-pretty/themes/monikai.css'
 import './Profile.css'
 
 const Profile = ({ isOpen, closeModal }) => {
@@ -21,7 +19,6 @@ const Profile = ({ isOpen, closeModal }) => {
                 <img src={user.picture} alt={user.name} />
                 <h2>{user.name}</h2>
                 <p>{user.email}</p>
-                <JSONPretty data={user} />
               </div>
             )}
           </div>
